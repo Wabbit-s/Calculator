7adea4ed49ac57ceccbeec49303c95d222edb8ce
Wabbit-s <63389643+Wabbit-s@users.noreply.github.com>
2021-03-17 13:41:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tkinter import *
+import math
 
 root = Tk()
 
 def click(event):
     global val
+    ops=["(",")","/","*","+","-"]
     text = event.widget.cget("text")
     print(text)
     if text == "=":
@@ -11,13 +13,73 @@ def click(event):
             value = int(val.get())
         else:
             try:
-                value = eval(scr.get())
-            except Exception as e:
-                value = "Invalid input"
-                print("Invalid Input")
+                if "cos" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))*math.pi/180
+                    value = round(math.cos(num),10)
 
-            except ZeroDivisionError as e:
-                value = "Invalid Input"
+                elif "sin" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))*math.pi/180
+                    value = round(math.sin(num),10)
+
+                elif "tan" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))*math.pi/180
+                    calc = round(math.tan(num),10)
+                    if calc>99999999999999:
+                        value = "Undefined"
+                    elif calc in [0.0,-0.0]:
+                        value = 0
+                    else:
+                        value = calc
+
+                elif "cot" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))*math.pi/180
+                    calc = round(1/math.tan(num),10)
+                    if calc>99999999999999:
+                        value = "Undefined"
+                    elif calc in [0.0,-0.0]:
+                        value = 0
+                    else:
+                        value = calc
+
+                elif "sec" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))*math.pi/180
+                    calc = round(1/math.cos(num),10)
+                    if calc>999999999999999:
+                        value = "Undefined"
+                    elif calc in [0.0,-0.0]:
+                        value = 0
+                    else:
+                        value = calc
+
+                elif "cosec" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))*math.pi/180
+                    calc = round(1/math.sin(num),10)
+                    if calc>999999999999999:
+                        value = "Undefined"
+                    elif calc in [0.0,-0.0]:
+                        value = 0
+                    else:
+                        value = calc
+
+                elif "log" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))
+                    print(num)
+                    if num<=0:
+                        value = "Undefined"
+                    else:
+                        value = round(math.log(num),10)
+
+                elif "sqrt" in scr.get():
+                    num = eval(''.join(i for i in scr.get() if i.isdigit() or i in ops))
+                    value = math.sqrt(num)
+
+                else:
+                    value = eval(scr.get())
+            except ZeroDivisionError:
+                value = "Undefined"
+                print("Invalid Input")
+            except Exception:
+                value = "Invalid input"
                 print("Invalid Input")
 
         val.set(value)
@@ -32,20 +94,22 @@ def click(event):
         scr.update()
 
 
-
-root.geometry("280x440+700+250")
-root.minsize(280, 440)
-root.maxsize(280, 440)
+#visual
+root.geometry("320x510+700+250")
+root.minsize(320, 510)
+root.maxsize(320, 510)
 root.title("Calculator")
 root.wm_iconbitmap('1.ico')
 
-
+#input
 val = StringVar()
 val.set("")
+
+#input textbox
 scr = Entry(root, textvar=val, font="arial 25 bold")
 scr.pack(fill=X, ipadx=5, padx=5, pady=12, anchor="n")
 
-
+#internal visual
 f = Frame(root, bg="black")
 
 b = Button(f, text="9", padx=20, pady=17, font="arial 15 bold")
@@ -57,6 +121,10 @@ b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
 b = Button(f, text="7", padx=20, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="cos", padx=20, pady=17, font="arial 15 bold")
 b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
@@ -77,6 +145,10 @@ b = Button(f, text="4", padx=20, pady=17, font="arial 15 bold")
 b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
+b = Button(f, text="sin", padx=20, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
 f.pack(padx=10)
 
 f = Frame(root, bg="black")
@@ -90,6 +162,10 @@ b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
 b = Button(f, text="1", padx=20, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="tan", padx=20, pady=17, font="arial 15 bold")
 b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
@@ -109,6 +185,10 @@ b = Button(f, text="*", padx=22, pady=17, font="arial 15 bold")
 b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
+b = Button(f, text="log", padx=20, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
 f.pack(padx=10)
 
 f = Frame(root, bg="black")
@@ -125,6 +205,10 @@ b = Button(f, text="=", padx=20, pady=17, font="arial 15 bold")
 b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
+b = Button(f, text="cot", padx=20, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
 f.pack(padx=10)
 
 f = Frame(root, bg="black")
@@ -138,6 +222,30 @@ b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
 b = Button(f, text=")", padx=21, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
+b = Button(f, text=".", padx=21, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
+f.pack(padx=10)
+
+f = Frame(root, bg="black")
+
+b = Button(f, text="sqrt", padx=19, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="sec", padx=23, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="cosec", padx=21, pady=17, font="arial 15 bold")
+b.pack(side=LEFT)
+b.bind("<Button-1>", click)
+
+b = Button(f, text="sec", padx=21, pady=17, font="arial 15 bold")
 b.pack(side=LEFT)
 b.bind("<Button-1>", click)
 
